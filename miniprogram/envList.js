e295835a1ab02e4222d9c27af043d38495d5e6b3
(Messi) Junlin Wu <tallmessiwu@qq.com>
2022-01-19 15:24:51 +0800

    Initial Commit

---
+++ miniprogram/envList.js
@@ -0,0 +1,6 @@
+const envList = [{"envId":"ucsdcssa-5gxqhwwc12d1b1bf","alias":"ucsdcssa"},{"envId":"music-imooc-4gw8og1u38e7ea15","alias":"music-imooc"}]
+const isMac = false
+module.exports = {
+    envList,
+    isMac
+}
---
+++ miniprogram/pages/home/home.js
@@ -0,0 +1,78 @@
+// pages/home/home.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    swiperImgs: [{
+        src: "/images/1.jpg",
+        title: "标题1"
+      },
+      {
+        src: "/images/2.jpg",
+        title: "标题2"
+      },
+      {
+        src: "/images/3.jpeg",
+        title: "标题3"
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
